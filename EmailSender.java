420ee3d70cdd9d0c6ec551b0e96de2e6a0d6aa53
Kaezar <rokki9@gmail.com>
2016-10-21 14:41:23 -0500

    Added Javadoc to EmailSender.Java

--- EmailSender.java
+++ EmailSender.java
@@ -13,6 +13,31 @@ import java.net.*;
 public class EmailSender {
 
     /**
+     * <h3>The entire program is run from the "main" method. </h3>
+     * It first establishes a TCP connection to the mail server by creating a Socket.
+     * <br>
+     * It then creates a BufferedReader from an InputStream to read a line at a time.
+     * <br>
+     * It then reads the initial response from the server and throws an exception if it doesn't receive a reply.
+     * <br>
+     * It then creates a reference to the socket's OutputStream which will be used to send data to the server.
+     * <br>
+     * It then sends a HELO command to greet the server (throws exception if invalid response).
+     * <br>
+     * It then sends the "MAIL TO" header information and the source email address (throws exception if invalid response).
+     * <br>
+     * It then sends the "RCPT TO" header information and the destination email address(throws exception if invalid response).
+     * <br>
+     * It then sends the "DATA" Command which indicates that the following lines are message data (throws exception if invalid response).
+     * <br>
+     * It then sends the message data line by line which includes "to", "from", and "subject" header lines which most email servers require to be a part of the message data.
+     * <br>
+     * When all message data has been sent, it then sends a line with a single period which tells the server that the message is over (throws exception if invalid response).
+     * <br>
+     * It then sends the "QUIT" command which tells the server to close its connection (throws exception if invalid response).
+     * <br>
+     * Finally, it closes the socket on the client side, terminating the connection.
+     * <br>
      * @param args the command line arguments
      * @throws java.lang.Exception
      */
@@ -20,16 +45,14 @@ public class EmailSender {
       // Establish a TCP connection with the mail server.
       Socket socket = new Socket("" + "aspmx.l.google.com", 25);
 
-      /** Create a BufferedReader to read a line at a time.
-       * 
-       */
+      // Create a BufferedReader to read a line at a time.
+     
       InputStream is = socket.getInputStream();
       InputStreamReader isr = new InputStreamReader(is);
       BufferedReader br = new BufferedReader(isr);
       
-      /** Read greeting from the server.
-      * 
-      */
+      // Read greeting from the server.
+  
       String response = br.readLine();
       System.out.println(response);
       if (!response.startsWith("220")) {
@@ -37,14 +60,12 @@ public class EmailSender {
          throw new Exception("220 reply not received from server.");
       }
 
-      /** Get a reference to the socket's output stream.
-       * 
-       */
+      // Get a reference to the socket's output stream.
+    
       OutputStream os = socket.getOutputStream();
 
-      /** Send HELO command and get server response.
-       * 
-       */
+      // Send HELO command and get server response.
+     
       String helocommand = "HELO kyle\r\n";
       System.out.print(helocommand);
       os.write(helocommand.getBytes("US-ASCII"));
@@ -55,9 +76,8 @@ public class EmailSender {
          throw new Exception("250 reply not received from server.");
       }
 
-      /** Send MAIL FROM command.
-       * This specifies the email address of the sender
-       */
+      // Send MAIL FROM command.
+  
       String mailfromcommand = "MAIL FROM: <Email@email.com>\r\n";
       System.out.print(mailfromcommand);
       os.write(mailfromcommand.getBytes("US-ASCII"));
@@ -68,9 +88,8 @@ public class EmailSender {
          throw new Exception("250 reply not received from server.");
       }
 
-      /** Send RCPT TO command.
-       * This specifies the email address to send the mail to.
-       */
+      // Send RCPT TO command.
+    
       String rcptcommand = "RCPT TO: <Email@email.com>\r\n";
       System.out.print(rcptcommand);
       os.write(rcptcommand.getBytes("US-ASCII"));
@@ -81,9 +100,8 @@ public class EmailSender {
          throw new Exception("250 reply not received from server.");
       }
 
-      /** Send DATA command.
-       * This lets the server know that the following lines are message data.
-       */
+      // Send DATA command.
+  
        String datacommand = "DATA\r\n";
       System.out.print(datacommand);
       os.write(datacommand.getBytes("US-ASCII"));
@@ -94,9 +112,8 @@ public class EmailSender {
          throw new Exception("354 reply not received from server.");
       }
 
-      /** Send message data.
-       * Includes to, from, and subject lines.
-       */
+      // Send message data.
+  
        String msgLine1 = "to: <Email@email.com>\r\n";
     System.out.print(msgLine1);
     os.write(msgLine1.getBytes("US-ASCII"));
@@ -113,9 +130,8 @@ public class EmailSender {
     System.out.print(msgLine4);
     os.write(msgLine4.getBytes("US-ASCII"));
     
-      /** End with line with a single period.
-    *
-    */
+      // End with line with a single period.
+   
       String msgLine5 = ".\r\n";
     System.out.print(msgLine5);
     os.write(msgLine5.getBytes("US-ASCII"));
@@ -126,9 +142,7 @@ public class EmailSender {
         throw new Exception("250 reply not received from server.");
     }
 
-      /** Send QUIT command.
-       * 
-       */
+      // Send QUIT command.
        String commandQUIT = "QUIT\r\n";
     System.out.print(commandQUIT);
     os.write(commandQUIT.getBytes("US-ASCII"));
@@ -138,9 +152,7 @@ public class EmailSender {
         socket.close();
         throw new Exception("221 reply not received from server.");
     }
-    /** Close the socket
-     * 
-     */
+    // Close the socket
     socket.close();
     }
     
